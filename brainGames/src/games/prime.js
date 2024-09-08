ee5c45a8474eb6c47076d4bf663b77e80f0a0f46
aleksei <jiunov@gmail.com>
2024-09-08 10:46:45 +0600

    test isPrime

--- brainGames/src/games/prime.js
+++ brainGames/src/games/prime.js
@@ -1,10 +1,18 @@
-import { isPrime } from 'mathjs';
+// import { isPrime } from 'mathjs';
 import getRandomInt from '../getRandomInt.js';
 
 export default () => {
   const ruleS = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
   const expressioN = getRandomInt(1, 200);
+
+  const isPrime = (num) => {
+    for (let i = 2, s = Math.sqrt(num); i <= s; i += 1) {
+      if (num % i === 0) return false;
+    }
+    return num > 1;
+  };
+
   const rigthAnsweR = isPrime(expressioN) ? 'yes' : 'no';
 
   return {
