c684539e6de663609cc035a4afce53bb08285a1b
aleksei <jiunov@gmail.com>
2024-09-15 08:59:44 +0600

    abstraction isEven

--- brainGames/src/games/even.js
+++ brainGames/src/games/even.js
@@ -1,10 +1,12 @@
 import getRandomInt from '../getRandomInt.js';
 
+const isEven = (num) => (num % 2 === 0);
+
 export default () => {
   const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
   const expression = getRandomInt(1, 100);
-  const rigthAnswer = (expression % 2 === 0) ? 'yes' : 'no';
+  const rigthAnswer = isEven(expression) ? 'yes' : 'no';
 
   return { rules, expression, rigthAnswer };
 };
