579f2cd5b508770818d646478e05857b4d881e87
aleksei <jiunov@gmail.com>
2024-09-22 06:11:45 +0600

    edit caclGame

--- brainGames/src/games/calc.js
+++ brainGames/src/games/calc.js
@@ -2,25 +2,25 @@ import getRandomInt from '../getRandomInt.js';
 
 const rules = () => 'What is the result of the expression?';
 
+const getRigthAnswer = (operation, firstNum, secondNum) => {
+  switch (operation) {
+    case '+':
+      return firstNum + secondNum;
+    case '-':
+      return firstNum - secondNum;
+    case '*':
+      return firstNum * secondNum;
+    default:
+      return NaN;
+  }
+};
+
 const calcGame = () => {
   const firstNum = getRandomInt(0, 10);
   const secondNum = getRandomInt(0, 10);
   const action = ['+', '-', '*'];
   const operation = action[getRandomInt(0, 3)];
   const expression = `${firstNum} ${operation} ${secondNum}`;
-
-  const getRigthAnswer = (operatioN, firstNuM, secondNuM) => {
-    switch (operatioN) {
-      case '+':
-        return firstNuM + secondNuM;
-      case '-':
-        return firstNuM - secondNuM;
-      case '*':
-        return firstNuM * secondNuM;
-      default:
-        return NaN;
-    }
-  };
 
   const rigthAnswer = String(getRigthAnswer(operation, firstNum, secondNum));
 
