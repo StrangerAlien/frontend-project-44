dff6eb845a6dc7893c8e7007f471dfe13b166795
aleksei <jiunov@gmail.com>
2024-10-02 19:44:10 +0600

    edit brain-progression

--- brainGames/src/games/progression.js
+++ brainGames/src/games/progression.js
@@ -3,14 +3,13 @@ import getRandomInt from '../getRandomInt.js';
 
 const rules = () => 'What number is missing in the progression?';
 
-const progressionGame = () => {
-  const start = getRandomInt(0, 50);
-  const step = getRandomInt(2, 6);
-  const length = getRandomInt(5, 10);
-  const end = start + length * step;
+const start = getRandomInt(0, 50);
+const step = getRandomInt(2, 6);
+const length = getRandomInt(5, 10);
+const end = start + length * step;
 
-  /* eslint-disable-next-line */
-  var progression = _.range(start, end, step);
+const progressionGame = () => {
+  const progression = _.range(start, end, step);
   const hiddenNum = getRandomInt(0, length - 1);
   const rigthAnswer = progression[hiddenNum];
   progression[hiddenNum] = '..';
